51f4cd54b49d09b016d609504703db88bcf914d6
JackAlexanderClark <jackalexanderclark@protonmail.com>
2022-06-01 16:04:14 +0000

    1

--- run.py
+++ run.py
@@ -4,13 +4,21 @@ Capital F for Flask because class name
 Create an instance of the class
 Naming convention for Flask 
 """
-
+import os
 from flask import Flask
 
 
-app = Flask(__name__)
+app = Flask(__name__)       # Instance of Flask class
 
 
-@app.route("/")
+@app.route("/")     # Decorators wrap around the function
 def index():
     return "HELLO WORLD IT IS I FLASK"
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),       # Using os module to .get the 'IP' environment
+        port=int(os.environ.get("PORT", "5000")), 
+        debug=True)     # debug=True allows easier debug later in dev
+    ) 
