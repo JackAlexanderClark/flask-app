f60029686ba51fcf56b26643c185372d30db89dc
JackAlexanderClark <jackalexanderclark@protonmail.com>
2022-06-12 12:23:07 +0000

    q

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ Create an instance of the class
 Naming convention for Flask 
 """
 import os
+import json
 from flask import Flask, render_template
 
 
@@ -13,23 +14,28 @@ app = Flask(__name__)
 # Adding an additional argument into return statement
 @app.route("/")
 def index():
-    return render_template("index.html", page_title="About")
+    return render_template("index.html")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+
+    # Using a with block, "r" means read-only, assigning to variable json_data
+    
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
-
+    return render_template("careers.html", page_title="Careers")
 
 if __name__ == "__main__":
     app.run(
